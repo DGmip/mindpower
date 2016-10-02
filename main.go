3d44bc85c3e7c5d49a195fa42d7ed746250931e2
ben <bwall256@gmail.com>
2016-10-02 18:05:16 +0200

    gitignore

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import  (
+	"net/http"
+	"github.com/golangdaddy/gf"
+	g "github.com/golangdaddy/gaml"
+)
+
+func main() {
+
+	handlers := map[string]gf.GFHandler{
+		"home": func (req *gf.Request) *gf.ResponseStatus {
+
+			ele := g.HTML().Add(
+				g.HEAD().Add(
+					g.TITLE("MindpowerStore"),
+				),
+				g.BODY().Add(
+					g.H(1, "Mindpower.store"),
+				),
+			)
+
+			return gf.Respond(ele)
+
+		},
+	}
+
+	root := gf.Router("mindpower", handlers)
+
+	root.Run("home", "GET")
+
+	panic(http.ListenAndServe(":80", root.HttpRouter()))
+
+}
